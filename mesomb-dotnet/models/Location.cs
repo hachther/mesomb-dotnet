6385c7772c7b7b86132993f7709f0a5bf563c2be
Edouard OWONA <69681767+The237@users.noreply.github.com>
2023-07-31 21:08:06 +0100

    Add code for Location model

--- mesomb-dotnet/models/Location.cs
+++ mesomb-dotnet/models/Location.cs
@@ -1,1 +1,17 @@
+using Newtonsoft.Json.Linq;
+
 namespace mesomb_dotnet.models;
+
+public class Location
+{
+    public String town { set; get; }
+    public String region { set; get; }
+    public String country { set; get; }
+
+    public Location(JObject data)
+    {
+        this.town = (String)data.GetValue("town");
+        this.country = (String)data.GetValue("country");
+        this.region = (String)data.GetValue("region");
+    }
+}
