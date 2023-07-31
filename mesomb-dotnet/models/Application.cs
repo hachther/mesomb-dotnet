37f7de770141a7145873cfa518f28cb143939804
Edouard OWONA <69681767+The237@users.noreply.github.com>
2023-07-31 17:21:39 +0100

    Create Application and ApplicationBalance classes

--- mesomb-dotnet/models/Application.cs
+++ mesomb-dotnet/models/Application.cs
@@ -1,1 +1,54 @@
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
 namespace mesomb_dotnet.models;
+
+/**
+ * Represent an instance of application
+*/
+public class Application
+{
+    public String key { get; set; }
+    public String logo { get; set; }
+    public ApplicationBalance[] balances { get; set; }
+    public String[] countries { get; set; }
+    public String description { get; set; }
+    public bool isLive { get; set; }
+    public String name { get; set; }
+    public dynamic security;
+    public String status { get; set; }
+    public String url { get; set; }
+
+    public Application(JObject data)
+    {
+        this.key = (String)data.GetValue("key");
+        this.logo = (String)data.GetValue("logo");
+
+        /** create the array of balances*/
+        JObject balances = (JObject)data.GetValue("balances");
+        this.balances = new ApplicationBalance[balances.Values().Count()];
+        for (int i = 0; i < balances.Values().Count(); i++)
+        {
+            this.balances[i] = new ApplicationBalance((JObject)balances[i]);
+        }
+
+        /** create the array of countries*/
+        JObject countries = (JObject)data.GetValue("countries");
+        this.countries = new String[countries.Values().Count()];
+        for (int i = 0; i < countries.Values().Count(); i++)
+        {
+            this.countries[i] = (String)(countries[i]);
+        }
+
+        this.description = (String)data.GetValue("description");
+        this.isLive = (bool)data.GetValue("is_live");
+        this.name = (String)data.GetValue("name");
+
+        this.security = (JObject)data.GetValue("security");
+
+        this.status = (String)data.GetValue("status");
+        this.url = (String)data.GetValue("url");
+    }
+
+}
--- mesomb-dotnet/models/ApplicationBalance.cs
+++ mesomb-dotnet/models/ApplicationBalance.cs
@@ -1,1 +1,21 @@
+using Newtonsoft.Json.Linq;
 namespace mesomb_dotnet.models;
+
+
+public class ApplicationBalance
+{
+    public string country;
+    public string currency;
+    public string provider;
+    public double value;
+    public string service_name;
+
+    public ApplicationBalance(JObject data)
+    {
+        this.country = (string)data.GetValue("country");
+        this.currency = (string)data.GetValue("currency");
+        this.provider = (string)data.GetValue("provider");
+        this.value = (double)data.GetValue("value");
+        this.service_name = (string)data.GetValue("service_name");
+    }
+}
