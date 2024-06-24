879001edbf3a8a28a1749fa4cd40fff2dd0bdb10
Nguetioofa <nguetioof@gmail.com>
2024-06-24 09:59:24 +0100

    Implemente  PaymentOperation

--- app/Program.cs
+++ app/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
-using mesomb_dotnet.Signature;
+using mesomb_dotnet;
+using mesomb_dotnet.operations;
+using mesomb_dotnet.util;
 namespace app;
 
 // class for futher testing needs of the final package
@@ -7,8 +9,19 @@ public class Program
 {
     static void Main(string[] args)
     {
-        String url = "http://127.0.0.1:8000/en/api/v1.1/payment/collect/";
-        String actual = Signature.signRequest("payment", "GET", url, new DateTime(1673827200000L), "fihser", credentials, null, null);
-        Console.WriteLine(url);
+        PaymentOperation paymentOperation = new(MeSomb.apiKey,MeSomb.clientKey,MeSomb.secretKey,MeSomb.apiBase,MeSomb.apiVersion);
+        Dictionary<string, object> parameters = new Dictionary<string, object>() 
+        {
+            {"payer", "670000000" },
+            { "amount", "100" },
+            { "nonce", RandomGenerator.Nonce() },
+            {"service", "MTN" } 
+        };
+
+
+        var response = (paymentOperation.MakeCollectAsync(parameters)).GetAwaiter().GetResult();
+        //String url = "http://127.0.0.1:8000/en/api/v1.1/payment/collect/";
+        //String actual = Signature.signRequest("payment", "GET", url, new DateTime(1673827200000L), RandomGenerator.Nonce(), credentials, null, null);
+        Console.WriteLine(response.reference);
     }
 }
